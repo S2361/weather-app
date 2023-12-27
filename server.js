c86239504774f60a2cfc05106ccb7936cc677482
patrickhong <path.plh@gmail.com>
2023-12-26 19:59:58 -0500

    Test

---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    // Check the URL
+    const filePath = req.url === '/' ? 'index.html' : req.url.slice(1);
+
+    // Get the file extension
+    const extname = path.extname(filePath);
+
+    // Set the content type based on the file extension
+    const contentType = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+    }[extname] || 'application/octet-stream';
+
+    // Read the file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // File not found
+                res.writeHead(404);
+                res.end('404 Not Found');
+            } else {
+                // Server error
+                res.writeHead(500);
+                res.end('500 Internal Server Error');
+            }
+        } else {
+            // Serve the file
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
